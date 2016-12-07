0b791e69a9c87559af6b70e42405f2eeb058a4da
Suyash12mahar <suyash12mah.uec2016@iitr.ac.in>
2016-12-07 22:30:21 +0530

    Changes to StringCipher.cs

--- Clipboarder.Encryption/StringCipher.cs
+++ Clipboarder.Encryption/StringCipher.cs
@@ -5,16 +5,10 @@ using System.IO;
 using System.Linq;
 namespace Clipboarder.Encryption {
     public static class StringCipher {
-        // This constant is used to determine the keysize of the encryption algorithm in bits.
-        // We divide this by 8 within the code below to get the equivalent number of bytes.
         private const int Keysize = 256;
-
-        // This constant determines the number of iterations for the password bytes generation function.
         private const int DerivationIterations = 1000;
 
         public static string Encrypt(string plainText, string passPhrase) {
-            // Salt and IV is randomly generated each time, but is preprended to encrypted cipher text
-            // so that the same Salt and IV values can be used when decrypting.  
             var saltStringBytes = Generate256BitsOfRandomEntropy();
             var ivStringBytes = Generate256BitsOfRandomEntropy();
             var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
@@ -29,7 +23,6 @@ namespace Clipboarder.Encryption {
                             using (var cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write)) {
                                 cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
                                 cryptoStream.FlushFinalBlock();
-                                // Create the final bytes as a concatenation of the random salt bytes, the random iv bytes and the cipher bytes.
                                 var cipherTextBytes = saltStringBytes;
                                 cipherTextBytes = cipherTextBytes.Concat(ivStringBytes).ToArray();
                                 cipherTextBytes = cipherTextBytes.Concat(memoryStream.ToArray()).ToArray();
@@ -44,14 +37,9 @@ namespace Clipboarder.Encryption {
         }
 
         public static string Decrypt(string cipherText, string passPhrase) {
-            // Get the complete stream of bytes that represent:
-            // [32 bytes of Salt] + [32 bytes of IV] + [n bytes of CipherText]
             var cipherTextBytesWithSaltAndIv = Convert.FromBase64String(cipherText);
-            // Get the saltbytes by extracting the first 32 bytes from the supplied cipherText bytes.
             var saltStringBytes = cipherTextBytesWithSaltAndIv.Take(Keysize / 8).ToArray();
-            // Get the IV bytes by extracting the next 32 bytes from the supplied cipherText bytes.
             var ivStringBytes = cipherTextBytesWithSaltAndIv.Skip(Keysize / 8).Take(Keysize / 8).ToArray();
-            // Get the actual cipher text bytes by removing the first 64 bytes from the cipherText string.
             var cipherTextBytes = cipherTextBytesWithSaltAndIv.Skip((Keysize / 8) * 2).Take(cipherTextBytesWithSaltAndIv.Length - ((Keysize / 8) * 2)).ToArray();
 
             using (var password = new Rfc2898DeriveBytes(passPhrase, saltStringBytes, DerivationIterations)) {
@@ -76,9 +64,8 @@ namespace Clipboarder.Encryption {
         }
 
         private static byte[] Generate256BitsOfRandomEntropy() {
-            var randomBytes = new byte[32]; // 32 Bytes will give us 256 bits.
+            var randomBytes = new byte[32];
             using (var rngCsp = new RNGCryptoServiceProvider()) {
-                // Fill the array with cryptographically secure random bytes.
                 rngCsp.GetBytes(randomBytes);
             }
             return randomBytes;
