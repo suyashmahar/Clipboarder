f35f8a392ce972c15cf2cc2aad2530482b5630c4
Suyash12mahar <suyash12mah.uec2016@iitr.ac.in>
2016-12-08 09:10:42 +0530

    Update ImageConversion

--- Clipboarder.Extension/ImageConversion.cs
+++ Clipboarder.Extension/ImageConversion.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Clipboarder.Extension {
-    class ImageConversion {
+    public class ImageConversion {
+
+        /// <summary>
+        /// Converts Image to Base64 string
+        /// </summary>
+        /// <param name="image">A System.Drawing.Image object</param>
+        /// <param name="format">Format for saving image to memory stream</param>
+        /// <returns>Base64 of an image</returns>
+        public static string ImageToBase64(Image image, System.Drawing.Imaging.ImageFormat format) {
+            using (MemoryStream ms = new MemoryStream()) {
+                // Convert Image to byte[]
+                image.Save(ms, format);
+                byte[] imageBytes = ms.ToArray();
+
+                // Convert byte[] to Base64 String
+                string base64String = Convert.ToBase64String(imageBytes);
+                return base64String;
+            }
+        }
+
+        /// <summary>
+        /// Returns Image from a base64 string
+        /// </summary>
+        /// <param name="base64String">string containing just base64 of image without prefixes</param>
+        /// <returns>Image</returns>
+        public static Image Base64ToImage(string base64String) {
+            // Convert Base64 String to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            MemoryStream ms = new MemoryStream(imageBytes, 0,
+              imageBytes.Length);
+
+            // Convert byte[] to Image
+            ms.Write(imageBytes, 0, imageBytes.Length);
+            Image image = Image.FromStream(ms, true);
+            return image;
+        }
     }
 }
---
+++ UnitTests/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clipboarder.Encryption;
+using System.Drawing;
+using System.IO;
+using Clipboarder.Extension;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace UnitTests {
+    class Program {
+        [STAThread]
+        static void Main(string[] args) {
+            Console.WriteLine("Unit Tests for Clipboarder v0.1b");
+            Console.WriteLine("Starting Tests...");
+            ImageConversionTests();
+        }
+
+        static void HashingTests() {
+            Console.WriteLine();
+            Console.WriteLine("Starting HashingTest...");
+            Stopwatch newStopWatch = new Stopwatch();
+            
+            for (int i = 8; i < 16; i++) {
+                newStopWatch.Start();
+                string passwordHashed = BCrypt.HashPassword("Th1$ 1& @ p@$$w0rd", BCrypt.GenerateSalt(i));
+                newStopWatch.Stop();
+                Console.WriteLine(String.Format("Time elapsed using salt of length {0} is {1} ticks or {2} ms.",i,newStopWatch.ElapsedTicks,newStopWatch.ElapsedMilliseconds));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("HashingTest Completed.");
+        }
+
+        static void DatabaseTests() {
+
+        }
+
+        static void ImageConversionTests() {
+            Image newImage = GetMeImage();
+            if (newImage == null) {
+                MessageBox.Show("This is not going to work");
+            }
+            string base64 = ImageConversion.ImageToBase64(newImage,newImage.RawFormat);
+            Console.WriteLine(base64);
+            newImage = ImageConversion.Base64ToImage(base64);
+            Clipboard.SetImage(newImage);
+        }
+        static Image GetMeImage() {
+            Image res = null;
+            Thread staThread = new Thread(x =>
+            {
+                try {
+                    res = Clipboard.GetImage();
+                } catch (Exception ex) {
+                    Console.WriteLine(ex.Message);
+                }
+            });
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+            staThread.Join();
+            return res;
+        }
+    }
+}
