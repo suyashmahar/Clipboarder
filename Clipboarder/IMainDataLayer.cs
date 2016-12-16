3a0f0e27d30276fa77fa586af6936c149371e895
Suyash12mahar <suyash12mah.uec2016@iitr.ac.in>
2016-12-16 23:33:21 +0530

    Add structs for content and interface IMainDataLayer

---
+++ Clipboarder/IMainDataLayer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Clipboarder {
+    interface IMainDataLayer {
+        void AddNewTextRow();
+        void AddNewImageRow();
+        //List GetImageContent();
+        //List GetTextContent();
+
+        
+    }
+}
